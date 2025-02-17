ef30d25260ffc5fc37005a651d82ff3aa9415a3e
artender <artem1347929@gmail.com>
2025-02-17 18:05:03 +0300

    ✨ Add HelpButton component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import PokeballButton from "./components/PokeballButton.vue";
+import HelpButton from "./components/HelpButton.vue";
 </script>
 
 <template>
+  <HelpButton />
   <PokeballButton />
 </template>
---
+++ src/components/HelpButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>?</button>
+</template>
+
+<style scoped>
+button {
+  background-color: #202020;
+  color: #3b3b3b;
+
+  font: inherit;
+
+  width: 3rem;
+  aspect-ratio: 1 / 1;
+
+  border: .125rem solid #3b3b3b;
+  border-radius: 50%;
+
+  cursor: pointer;
+
+  transition: filter .25s, transform .125s;
+}
+
+button:hover {
+  filter: brightness(1.1);
+}
+
+button:active {
+  transform: scale(.925);
+}
+</style>
